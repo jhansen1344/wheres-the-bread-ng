dc22bc730738a8a201e323691f2078f43eee219b
Jeremy Hansen <jhansen1344@gmail.com>
2020-10-02 16:48:19 -0400

    route guard added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,11 @@ import { HomeComponent } from './home/home.component';
 import { ItemsComponent } from './items/items.component';
 import { SubactivitiesComponent } from './subactivities/subactivities.component';
 import { PlanComponent } from './plan/plan.component';
+import { AuthGuard } from './_guards/auth.guard';
 
 export const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'items', component: ItemsComponent},
+  {path: 'items', component: ItemsComponent, canActivate: [AuthGuard]},
   {path: 'subactivities', component: SubactivitiesComponent},
   {path: 'plan', component: PlanComponent},
   {path: '**', component: HomeComponent, pathMatch: 'full'},
