9c7ad1e7cb4f5336bb19ad25189a54b2b7c4c6d4
Jeremy Hansen <jhansen1344@gmail.com>
2020-10-07 21:05:12 -0400

    item model added, itemservice created

--- src/app/_services/account.service.ts
+++ src/app/_services/account.service.ts
@@ -3,12 +3,13 @@ import {HttpClient} from '@angular/common/http';
 import {map} from 'rxjs/operators';
 import { User } from '../_models/user';
 import { ReplaySubject } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
-  baseUrl ='https://localhost:44366/api/';
+  baseUrl = environment.apiUrl;
   private currentUserSource = new ReplaySubject<User>(1);
   currentUser$ = this.currentUserSource.asObservable();
   constructor(private http: HttpClient) { }
---
+++ src/app/_services/items.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Item } from '../_models/item';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ItemsService {
+  baseUrl = environment.apiUrl;
+  constructor(private http: HttpClient) {
+   }
+
+   getItems(){
+     return this.http.get<Item[]>(this.baseUrl + 'item');
+   }
+}
