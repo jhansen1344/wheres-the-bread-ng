7c1f00fbdd85899548b02ffb267a343a1322ca31
Jeremy Hansen <jhansen1344@gmail.com>
2020-10-17 16:52:05 -0400

    delete item methods added to item-list component

--- src/app/items/item-list/item-list.component.ts
+++ src/app/items/item-list/item-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Item } from 'src/app/_models/item';
 import { ItemsService } from 'src/app/_services/items.service';
 import { Observable } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-item-list',
@@ -14,6 +15,12 @@ export class ItemListComponent implements OnInit {
 
   ngOnInit(): void {
     this.items$ = this.itemService.getItems();
+  }
+
+  deleteItem(id: number){
+    this.itemService.deleteItem(id).subscribe(() =>{
+      this.items$ = this.itemService.getItems();
+    })
   }
 
 }
