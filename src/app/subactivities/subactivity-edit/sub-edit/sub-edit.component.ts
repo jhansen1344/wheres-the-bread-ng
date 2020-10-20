afd034eb2c815fb0a891558562ec7119213fcd91
Jeremy Hansen <jhansen1344@gmail.com>
2020-10-20 19:08:16 -0400

    subactivity edit started

--- src/app/subactivities/subactivity-edit/sub-edit/sub-edit.component.ts
+++ src/app/subactivities/subactivity-edit/sub-edit/sub-edit.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, HostListener } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { SubActivityDetail } from 'src/app/_models/subActivityDetail';
+import { SubService } from 'src/app/_services/sub.service';
+import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-sub-edit',
@@ -7,9 +12,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SubEditComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('editForm')editForm: NgForm;
+  sub: SubActivityDetail;
+  items: any[];
+  @HostListener('window:beforeunload', ['$event']) unloadNotification($event: any){
+    if(this.editForm.dirty){
+      $event.returnValue =  true;
+    }
+  }
+  constructor(private subService: SubService, private route: ActivatedRoute, private toastr: ToastrService) { }
 
   ngOnInit(): void {
+    this.loadSub();
+  }
+
+  loadSub(){
+    this.subService.getSub(this.route.snapshot.paramMap.get('id')).subscribe(sub => {
+      this.sub = sub;
+    });
+  }
+
+  updateItem(){
+   this.subService.updateSub(this.sub).subscribe(() => {
+    this.toastr.success('Activity Updated Successfully');
+    this.editForm.reset(this.sub);
+   })
   }
 
 }
